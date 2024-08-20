a749f40ce7f32d6965397d02f978f3949d06fac5
camanu2 <143728824+camanu2@users.noreply.github.com>
2024-08-20 12:13:44 -0300

    5 videos feito

---
+++ camanu/scripts/main.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+@export var bug_scene: PackedScene
+var score
+
+
+
+func gamer_over():
+	$BugTimer.stop()
+	$ScoreTimer.stop()
+	$HUD.show_gamer_over()
+	
+func new_game():
+	$StartTimer.start()
+	$player.start_pos($StartPosition.position)
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+
+
+
+
+func _on_scare_timer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_start_timer_timeout():
+	$BugTimer.start()
+	$ScareTimer.start()
+
+func _on_bug_timer_timeout():
+	var bug = bug_scene.instantiate()
+	var bug_location = $BugPath/ButPathLocation
+	bug_location.progress_ratio = randf()
+	
+	
+	var direction = bug_location.rotation + PI / 2
+	bug.position = bug_location.position
+	direction += randf_range(-PI /4, PI/4)
+	bug.rotation = direction
+	
+	var velocity =Vector2(randf_range(150.0,250.0), 0.0)
+	bug.linear_velocity = velocity.rotated(direction)
+	add_child(bug)
