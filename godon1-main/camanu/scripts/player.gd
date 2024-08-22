c02d8446c97dfe149d1ddaf239fdb957c380c4c4
camanu2 <143728824+camanu2@users.noreply.github.com>
2024-08-22 09:07:50 -0300

    Add files via upload

--- godon1-main/camanu/scripts/player.gd
+++ godon1-main/camanu/scripts/player.gd
@@ -1,10 +1,17 @@
 extends Area2D
 
+signal hit
 const SPEED := 400
-var screen_size
+
+# Called when the node
+@onready var screen_size = get_viewport_rect().size
+@onready var collision = $collision
+@onready var anim = $anim
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size= get_viewport_rect().size
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,5 +21,36 @@ func _process(delta):
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
 		
+		if velocity.x != 0:
+			anim.play("move")
+		elif velocity.y > 0:
+			anim.play("move_up")
+		elif velocity.y < 0:
+			anim.play("move_down")
+		else:
+			anim.play("idle")
+			
+		#if velocity.x > 0:
+			#anim.flip_h = false
+		#else:
+			#anim.flip_h = true
+			
+			anim.flip_h = true if velocity.x > 0 else false
+			
+			
+			
+		
+		
 		position += velocity * delta
 		position = position.clamp(Vector2.ZERO, screen_size) 
+
+#Verificação da colisão do player com os bugs
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	collision.set_deferred("disabled", true)
+	
+func start_pos(pos) :
+	position = pos
+	show()
+	collision.disabled = false
